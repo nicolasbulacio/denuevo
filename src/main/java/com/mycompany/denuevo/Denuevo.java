0c5c64cb89be7695b111ee8f90f59904c3c14569
bulacio nicolas <93942151+nicolasbulacio@users.noreply.github.com>
2023-08-09 15:48:32 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/denuevo/Denuevo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.denuevo;
+
+/**
+ *
+ * @author nicolas
+ */
+public class Denuevo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
